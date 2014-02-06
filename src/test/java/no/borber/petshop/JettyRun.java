1944f8aee074ccc3dd2123c3617277887d366ec5
Andreas Berre <andreas.berre@bouvet.no>
2014-02-06 13:06:22 +0100

    grunnleggende struktur med hello world fra jetty

---
+++ src/test/java/no/borber/petshop/JettyRun.java
@@ -0,0 +1,20 @@
+package no.borber.petshop;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class JettyRun {
+
+    public static void main(String... args) throws Exception {
+
+        Server server = new Server(9090);
+
+        WebAppContext context = new WebAppContext("src/main/webapp", "/");
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+
+        server.setStopAtShutdown(true);
+    }
+}
