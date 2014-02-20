d88adeff3e268708ab27856be1e0b1566fea3251
Andreas Berre <andreas.berre@bouvet.no>
2014-02-20 11:14:40 +0100

    fixed #1. Implementasjon av rest-api for autentisering

---
+++ src/main/java/no/borber/monsterbutikken/autentisering/Kunde.java
@@ -0,0 +1,13 @@
+package no.borber.monsterbutikken.autentisering;
+
+public class Kunde {
+    private String kundenavn;
+
+    public Kunde(String kundenavn) {
+        this.kundenavn = kundenavn;
+    }
+
+    public String getKundenavn() {
+        return kundenavn;
+    }
+}
